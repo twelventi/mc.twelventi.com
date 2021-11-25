9ad7d3a5947c227a0d06ca34799efb08d605d466
twelventi <1220dab@gmail.com>
2021-11-24 20:44:40 -0500

    .

--- bot/handle_messages.py
+++ bot/handle_messages.py
@@ -7,7 +7,7 @@ token_path = f"{dir_path}/../simple_server/api/token.txt"
 async def handle_message(m):
     if m.channel.name == "invite_links":
         if m.content == "!cil":
-            token = ''.join([random.random(0,9) for _ in range(32)])
+            token = ''.join([ str(random.randint(0,9)) for _ in range(32)])
             m.channel.send(f"https://mc.twelventi.com/?it={token}")
             with open(token_path, 'a') as f:
                 f.write(f"{token}\n")
